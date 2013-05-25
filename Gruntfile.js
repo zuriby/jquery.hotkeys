f8225d120c7bce1e92130ad2ee8a32a3973c395e
Tim Branyen <tim@tabdeveloper.com>
2013-05-25 14:07:35 -0400

    added in grunt for linting, eventually testing

---
+++ Gruntfile.js
@@ -0,0 +1,16 @@
+module.exports = function(grunt) {
+
+  // Configuration.
+  grunt.initConfig({
+    jshint: {
+      files: ["jquery.hotkeys.js"]
+    }
+  });
+
+  // Task loading.
+  grunt.loadNpmTasks("grunt-contrib-jshint");
+
+  // Task registration.
+  grunt.registerTask("default", ["jshint"]);
+
+};
