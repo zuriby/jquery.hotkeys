cf24c91990f5d9392e0bc3175c33eba36d95f448
Robert Pocklington <rpocklin@gmail.com>
2014-08-12 09:01:21 +1000

    Adding grunt-jsbeautify.

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,6 +2,31 @@ module.exports = function(grunt) {
 
   // Configuration.
   grunt.initConfig({
+    // verifies we have formatted our js and HTML according to our style conventions
+    jsbeautifier: {
+      files: ['Gruntfile.js', 'jquery.hotkeys.js', 'test/spec/**/*.js'],
+      options: {
+        mode: 'VERIFY_ONLY',
+        js: {
+          'indent_size': 2,
+          'indent_char': ' ',
+          'indent_level': 0,
+          'indent_with_tabs': false,
+          'preserve_newlines': true,
+          'max_preserve_newlines': 2,
+          'jslint_happy': false,
+          'brace_style': 'end-expand',
+          'indent_scripts': 'keep',
+          'keep_array_indentation': true,
+          'keep_function_indentation': false,
+          'space_before_conditional': true,
+          'break_chained_methods': false,
+          'eval_code': false,
+          'unescape_strings': false,
+          'wrap_line_length': 0
+        }
+      }
+    },
     jshint: {
       options: {
         "undef": true,
@@ -34,12 +59,15 @@ module.exports = function(grunt) {
 
   grunt.loadNpmTasks('grunt-contrib-watch');
 
-  // Task loading.
+  // Beautify javascript
+  grunt.loadNpmTasks("grunt-jsbeautifier");
+
+  // Task loading
   grunt.loadNpmTasks("grunt-contrib-jshint");
 
-  // tests
+  // Running tests
   grunt.loadNpmTasks('grunt-contrib-jasmine');
 
   // Task registration.
-  grunt.registerTask("default", ["jshint", "jasmine"]);
+  grunt.registerTask("default", ["jsbeautifier", "jshint", "jasmine"]);
 };
