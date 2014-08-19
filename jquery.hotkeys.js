zuriby/jquery.hotkeys

commit 8b73fdc804b26f69b4636634f0873c1ea2443231

fix keypress on special keys and other events on characters

jquery.hotkeys.js
@@ -58,10 +58,10 @@
 				jQuery.inArray(event.target.type, textAcceptingInputTypes) > -1 ) ) {
 				return;
 			}
-			
-			// Keypress represents characters, not special keys
-			var special = event.type !== "keypress" && jQuery.hotkeys.specialKeys[ event.which ],
-				character = String.fromCharCode( event.which ).toLowerCase(),
+
+			var special = jQuery.hotkeys.specialKeys[ event.keyCode ],
+				// character codes are available only in keypress
+				character = event.type === "keypress" && String.fromCharCode( event.which ).toLowerCase(),
 				modif = "", possible = {};
 
 			// check combinations (alt|ctrl|shift+anything)
@@ -84,8 +84,9 @@
 
 			if ( special ) {
 				possible[ modif + special ] = true;
+			}
 
-			} else {
+			if ( character ) {
 				possible[ modif + character ] = true;
 				possible[ modif + jQuery.hotkeys.shiftNums[ character ] ] = true;
 

commit 26c02d0ecc44a4eebb627db9fd1b4af36e75d503

Fixing keypress binding issue introduced in 8b73fdc and 3cae520.

jquery.hotkeys.js
@@ -135,14 +135,15 @@
       keys = handleObj.data.keys.toLowerCase().split(" ");
 
     handleObj.handler = function(event) {
-      // Don't fire in text-accepting inputs that we didn't directly bind to
+      //      Don't fire in text-accepting inputs that we didn't directly bind to
       if (this !== event.target && (/textarea|select/i.test(event.target.nodeName) ||
         (jQuery.hotkeys.options.filterTextInputs &&
           jQuery.inArray(event.target.type, jQuery.hotkeys.textAcceptingInputTypes) > -1))) {
         return;
       }
 
-      var special = jQuery.hotkeys.specialKeys[event.keyCode],
+      var special = event.type !== "keypress" && jQuery.hotkeys.specialKeys[event.which],
+
         character = String.fromCharCode(event.which).toLowerCase(),
         modif = "",
         possible = {};
@@ -158,8 +159,7 @@
       if (special) {
         possible[modif + special] = true;
       }
-
-      if (character) {
+      else {
         possible[modif + character] = true;
         possible[modif + jQuery.hotkeys.shiftNums[character]] = true;
 